46a3cb0be0e45cc453990a8613bec1ef1b26410a
Irving Beier <irving.beier0802@gmail.com>
2024-04-10 15:27:33 +1000

    init game defense

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    public Text textScore;
+
+    public bool scoreBool = false;
+
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+        GetComponent<Rigidbody>().AddForce(4000 * transform.forward);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject hitTemp = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(hitTemp, 1);
+        if (collider.gameObject.tag == "enemy" || collider.gameObject.tag == "boss")
+        {
+            EnemyBehaviour enemy = collider.gameObject.GetComponent<EnemyBehaviour>();
+            enemy.health--;
+            enemy.slider.value= enemy.health;
+            if(enemy.health == 0){
+            Destroy(collider.gameObject);
+            TowerBehaviour.score++;
+            textScore.text = "score: " + TowerBehaviour.score;
+            }
+        }
+        Destroy(gameObject);
+    }
+}
+
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EnemySpawn : MonoBehaviour
+{
+    bool isSpawning = false;
+    public GameObject[] enemies;
+    public GameObject turret;
+
+    IEnumerator SpawnObject(int index, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        GameObject temp = Instantiate(enemies[index], transform.position, transform.rotation);
+        temp.GetComponent<EnemyBehaviour>().player = turret;
+        isSpawning = false;
+    }
+
+    void Update()
+    {
+        if (!isSpawning)
+        {
+            isSpawning = true;
+            int enemyIndex = Random.Range(0, enemies.Length);
+            StartCoroutine(SpawnObject(enemyIndex, Random.Range(3, 8)));
+        }
+    }
+
+    public static IEnumerator WaitForRestart()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
